941fe681b7e8ede867aa0871d8e1d77467045dc0
ryan2156 <adasdsd60112@gmail.com>
2023-12-25 17:16:03 +0800

    角色可以成功被操作

--- game/main.py
+++ game/main.py
@@ -63,7 +63,7 @@ class Ball:
         self.x += self.vx
         self.y += self.vy
         
-        print(self.x,self.vx,self.y,self.vy)
+        # print(self.x,self.vx,self.y,self.vy)
 
         # 檢查是否碰撞邊界
         if self.x < 0 or self.x > WIDTH:
@@ -84,13 +84,27 @@ class Ball:
         # 檢查碰撞點是否在 character 物件的範圍內
         if d <= r:
             # 反彈球的方向
-            self.vx = -self.vx
-            self.vy = -self.vy+0.5
+            self.vx = -1.0069*self.vx 
+            self.vy = -1.0069*self.vy
+            
+            # 限制球速
+            max_speed = 15.6
+            if self.vx < 0:
+                self.vx = -min(abs(self.vx), max_speed)
+            else:
+                self.vx = min(abs(self.vx), max_speed)
+
+            if self.vy < 0:
+                self.vy = -min(abs(self.vy), max_speed)
+            else:
+                self.vy = min(abs(self.vy), max_speed)
 
             # 檢查碰撞點是否在 character 物件的範圍內
             return pygame.Rect.collidepoint(other.rect, (self.x, self.y))
         else:
             return False
+
+        
 
 
 class Floor:
@@ -104,42 +118,35 @@ class Floor:
 
 
 class Controller:
-    def __init__(self, speed=5):
-        # 控制器的初始位置
-        self.x = 400
-        self.y = 200
-        #移動速度
+    def __init__(self, character, speed=5):
+        self.character = character
         self.speed = speed
-        # 新增殺球的相關屬性
-        self.ball_x = self.x
-        self.ball_y = self.y - 20  # 在角色頭上的位置
 
-    #根據按下或釋放的按鍵觸發相應的移動或停止操作。
     def handle_event(self, event):
         if event.type == KEYDOWN:
             if event.key == pygame.K_LEFT:
                 self.move_left()
-            elif event.key == pygame.K_RIGHT: 
+            elif event.key == pygame.K_RIGHT:
                 self.move_right()
         elif event.type == KEYUP:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                 self.stop()
-    # 移動控制
+
     def move_left(self):
-        self.x -= self.speed
+        self.character.vx = -self.speed
 
     def move_right(self):
-        self.x += self.speed
+        self.character.vx = self.speed
 
     def stop(self):
-        self.x = 0
+        self.character.vx = 0
 
-    # 跳躍控制
     def update(self):
+        self.character.update()
 
-        # 檢查是否碰撞邊界
-        if self.x < 0 or self.x > WIDTH:
-            self.x = 0
+        if self.character.x < 0 or self.character.x > WIDTH:
+            self.character.vx = 0
+            self.character.x = max(0, min(self.character.x, WIDTH))
             
 class Window:
     def __init__(self):
@@ -147,12 +154,12 @@ class Window:
         self.height = 600
 
         self.surface = pygame.display.set_mode((self.width, self.height))
-        self.floor = Floor(800, 200, (0, 0, 255))  # 新增地板物件
+        self.floor = Floor(800, 150, (0, 0, 255))  # 新增地板物件
         
     def draw(self, character: Character, ball: Ball):
         self.surface.fill((0, 0, 0))
 
-        self.floor.draw(self.surface, 400)  # 繪製地板
+        self.floor.draw(self.surface, 450)  # 繪製地板
         character.draw(self.surface)
         ball.draw(self.surface)
 
@@ -162,21 +169,38 @@ def check_collide(ball: Ball, character: Character):
     # 檢查球是否碰撞角色
     return ball.collide(character)
 
+def reset_game(character, ball):
+    # 重新開始遊戲，將球的位置和速度重新設定為初始值
+    ball.x = 400
+    ball.y = 100
+    ball.vx = 0
+    ball.vy = 0
+    # 重新開始遊戲，將角色的位置和速度重新設定為初始值
+    character.x = 475
+    character.vx = 0
+    
 def main():
     pygame.init()  # 初始化 Pygame
 
     window = Window()  # 建立視窗物件
 
-    ball = Ball(400, 200, 50, (0, 255, 0))  # 建立球物件
-    character = Character(475, 525, 75, (255, 0, 0))  # 建立角色物件
+    ball = Ball(400, 100, 50, (0, 255, 0))  # 建立球物件
+    character = Character(475, 375, 75, (255, 0, 0))  # 建立角色物件
+    controller = Controller(character)
+    
     # 將 character 物件傳遞給 ball 物件
     ball.character = character
     while True:  # 遊戲迴圈
         time.sleep(0.016)
         for event in pygame.event.get():  # 處理事件
+            print(event)
             if event.type == QUIT:
                 pygame.quit()
                 sys.exit()
+            elif event.type == KEYDOWN:
+                if event.key == pygame.K_r:  # 按下 'r' 鍵重置遊戲
+                    reset_game(character, ball)
+            controller.handle_event(event)
 
         ball.update()  # 更新球的狀態
         character.update()  # 更新角色的狀態（如果有）
@@ -190,6 +214,8 @@ def main():
         window.draw(character, ball)  # 繪製畫面
 
         pygame.display.update()  # 更新視窗顯示
-
+        # 檢查是否需要重新開始遊戲
+        if ball.y > HEIGHT:
+            reset_game(character, ball)
 if __name__ == "__main__":
     main()
